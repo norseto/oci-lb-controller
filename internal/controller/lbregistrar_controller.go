38919cfd1c4fd90edad15339ef3d37286c15e5ed
Norihiro Seto <norihiros@acm.org>
2024-02-15 04:02:36 +0900

    [WIP] Add rbac

--- internal/controller/lbregistrar_controller.go
+++ internal/controller/lbregistrar_controller.go
@@ -44,6 +44,7 @@ type LBRegistrarReconciler struct {
 //+kubebuilder:rbac:groups=nodes.peppy-ratio.dev,resources=lbregistrars,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=nodes.peppy-ratio.dev,resources=lbregistrars/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=nodes.peppy-ratio.dev,resources=lbregistrars/finalizers,verbs=update
+//+kubebuilder:rbac:groups="",resources=node,verbs=get,list,watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
