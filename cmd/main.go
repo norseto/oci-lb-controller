87224d066f12d306bf525af413bc0c8cdefdafdb
Norihiro Seto <norihiros@acm.org>
2024-04-05 01:42:19 +0900

    Add webhook

--- cmd/main.go
+++ cmd/main.go
@@ -144,6 +144,12 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "LBRegistrar")
 		os.Exit(1)
 	}
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err = (&nodesv1alpha2.LBRegistrar{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "LBRegistrar")
+			os.Exit(1)
+		}
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
