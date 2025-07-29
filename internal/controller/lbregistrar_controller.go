414f44423e19669c45f57f562bf897b331e3b126
Norihiro Seto <norseto@users.noreply.github.com>
2025-07-29 15:58:33 +0900

    chore: remove leftover TODO comments

--- internal/controller/lbregistrar_controller.go
+++ internal/controller/lbregistrar_controller.go
@@ -60,13 +60,8 @@ type LBRegistrarReconciler struct {
 //+kubebuilder:rbac:groups="",resources=endpoints,verbs=get;list;watch
 //+kubebuilder:rbac:groups="",resources=pods,verbs=get;list;watch
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the LBRegistrar object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
+// Reconcile is part of the main kubernetes reconciliation loop which ensures
+// the cluster state reflects the LBRegistrar specification.
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.17.0/pkg/reconcile
 func (r *LBRegistrarReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
