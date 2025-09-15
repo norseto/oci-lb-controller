fd89a31164e7c499eb00d818961de9ea568d8594
Norihiro Seto <norihiros@acm.org>
2025-09-15 20:43:47 +0900

    Fix directory permission

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 sudo chown -R vscode:vscode \
-  /home/vscode/.aws /home/vscode/.kube /home/vscode/.cache/JetBrains \
+  /home/vscode/.aws /home/vscode/.kube /home/vscode/.cache \
   /usr/local/go /go/bin /go/pkg /home/vscode/.gocache
