54fe4dd81131ccdb3ba9d34cfdf6269f07003d25
Norihiro Seto <norihiros@acm.org>
2025-09-25 03:11:47 +0900

    Hold codex history

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,3 +3,5 @@
 sudo chown -R vscode:vscode \
   /home/vscode/.aws /home/vscode/.kube /home/vscode/.cache \
   /usr/local/go /go/bin /go/pkg /home/vscode/.gocache
+
+sudo chown -R $(id -u):$(id -g) $HOME/.codex
