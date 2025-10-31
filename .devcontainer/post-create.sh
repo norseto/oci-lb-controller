f03aa1d17171eafb3bd9d4353dc52233419aaf25
Norihiro Seto <norihiros@acm.org>
2025-10-31 18:10:34 +0900

    Add CC volume mount

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,4 +4,4 @@ sudo chown -R vscode:vscode \
   /home/vscode/.aws /home/vscode/.kube /home/vscode/.cache \
   /usr/local/go /go/bin /go/pkg /home/vscode/.gocache
 
-sudo chown -R $(id -u):$(id -g) $HOME/.codex
+sudo chown -R $(id -u):$(id -g) $HOME/.codex $HOME/.claude
